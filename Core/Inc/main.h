02d7eb20133a83fbc54e49cda44bf90c273da77f
Lauris <lauris.klim@gmail.com>
2025-12-25 22:52:52 +0200

    initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define gpio_button1_Pin GPIO_PIN_10
+#define gpio_button1_GPIO_Port GPIOE
+#define gpio_button2_Pin GPIO_PIN_11
+#define gpio_button2_GPIO_Port GPIOE
+#define gpio_button3_Pin GPIO_PIN_12
+#define gpio_button3_GPIO_Port GPIOE
+#define gpio_led1_Pin GPIO_PIN_13
+#define gpio_led1_GPIO_Port GPIOE
+#define gpio_led2_Pin GPIO_PIN_14
+#define gpio_led2_GPIO_Port GPIOE
+#define gpio_led3_Pin GPIO_PIN_15
+#define gpio_led3_GPIO_Port GPIOE
+#define gpio_rs485de_Pin GPIO_PIN_7
+#define gpio_rs485de_GPIO_Port GPIOD
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
