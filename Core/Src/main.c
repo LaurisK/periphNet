e8aa33b08ed48bca7e01b141bb11d7ffd3d9663b
Lauris <lauris.klim@gmail.com>
2025-12-27 00:33:15 +0200

    flash still passes after autogenerate.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -101,9 +101,9 @@ int main(void)
   MX_SPI2_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
-/* USER CODE BEGIN 2 */
+  /* USER CODE BEGIN 2 */
 
-/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
   /* Init scheduler */
   osKernelInitialize();
